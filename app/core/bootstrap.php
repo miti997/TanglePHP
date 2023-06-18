5241d3051352e073b029f33839616a00807d93de
Dumitru <miti_or_2010@yahoo.com>
2023-06-18 11:30:49 +0300

    Moved config file in it's own folder.

--- app/core/bootstrap.php
+++ app/core/bootstrap.php
@@ -11,11 +11,12 @@ use app\App;
 define('DS', DIRECTORY_SEPARATOR);
 define('SRC', ROOT . 'app' . DS . 'src' . DS);
 define('CORE', ROOT . 'app' . DS . 'core' . DS);
+define('CONFIG', ROOT . 'app' . DS . 'config' . DS);
 define('BUILT_COMPONENTS', SRC . 'built_templates' .  DS);
 define('TEMPLATES', SRC . 'templates' . DS);
 define('COMPONENTS', SRC . 'components' . DS);
 
-$config = require_once CORE . 'config.php';
+$config = require_once CONFIG . 'config.php';
 
 require_once CORE . 'autoloader.php';
 require_once CORE . 'debug.php';
